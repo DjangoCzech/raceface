779347fcbbb07666086bb9781208073c9ba00ae3
DjangoCzech <janpanus@gmail.com>
2023-06-12 14:04:41 +0200

    update path to models

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,10 @@ const divCanvas = document.getElementById("divCanvas");
 var faceDirection = "";
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("../models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("../models"),
 ]).then(startVideo);
 
 function startVideo() {
